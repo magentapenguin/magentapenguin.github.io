05fe382f868e722535e0c1690eb2692a4571b2fe
magentapenguin <83044109+magentapenguin@users.noreply.github.com>
2025-04-30 18:32:01 +0000

    Add favicon generation and update links.json with icon paths

--- iconembed.py
+++ iconembed.py
@@ -1,4 +1,4 @@
-import requests, bs4, json, os, secrets, mimetypes
+import requests, bs4, json, os, secrets, mimetypes, urllib.parse
 
 os.chdir(os.path.dirname(__file__))
 
@@ -17,10 +17,13 @@ def find_icon(icon_url, prefered_size=64):
             icons = soup.find_all('link', rel='shortcut icon')
         if not icons:
             icons = soup.find_all('link', rel='apple-touch-icon')
-        for icon in icons:
-            sizes = icon.get('sizes')
-            if sizes and int(sizes.split('x')[0]) >= prefered_size:
-                return icon.get('href')
+        valid_icons = {icon['href']: abs(int(icon.get('sizes', '0x0').split('x')[0]) - prefered_size) for icon in icons}
+        if valid_icons:
+            best_icon = min(valid_icons, key=valid_icons.get)
+            return best_icon
+        response = requests.get(urllib.parse.urljoin(icon_url, '/favicon.ico'), timeout=5)
+        if response.ok:
+            return urllib.parse.urljoin(icon_url, '/favicon.ico')
         return None
     except Exception as e:
         print(f"Error finding icon: {e}")
@@ -36,7 +39,10 @@ def download_icon(icon_url, filename, ext=None):
         response = requests.get(icon_url, timeout=5)
         if response.status_code == 200:
             if ext is None:
-                ext = mimetypes.guess_extension(response.headers['Content-Type'])
+                if icon_url.endswith('.ico'):
+                    ext = '.ico'
+                else:
+                    ext = mimetypes.guess_extension(response.headers['Content-Type'], strict=False)
             with open(f"{filename}{ext}", 'wb') as f:
                 f.write(response.content)
                 
@@ -57,11 +63,15 @@ for link in links['data']:
         icon = find_icon(icon_url)
         print(f"Found icon: {icon}")
         if icon:
+            icon = urllib.parse.urljoin(icon_url, icon)
             icon_name = os.path.basename(icon)
-            filename = os.path.join('public/favicons', f"{icon_name}")
+            filename = os.path.join('public/favicons', f"{secrets.token_hex(8)}")
             download_icon(icon, filename)
-            link['icon'] = filename
+            links['data'][links['data'].index(link)]['icon'] = '/'+filename
             print(f"Downloaded icon: {filename}")
         else:
             print(f"No icon found for {link['name']}")
-    
+
+with open('src/links.json', 'w') as f:
+    json.dump(links, f, indent=4)
+    print("Updated links.json with icon paths.")
